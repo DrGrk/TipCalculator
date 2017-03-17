98c7508750d2dca0d4a95eabea43ff1c99f033d5
David Guichon <davidrguichon@gmail.com>
2017-03-17 11:28:16 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by David Guichon on 2017-03-17.
+//  Copyright © 2017 David Guichon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
